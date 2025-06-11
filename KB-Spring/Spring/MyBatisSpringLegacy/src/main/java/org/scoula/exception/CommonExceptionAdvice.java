18030fcc4323b997659cfd8a20053c7320d08f5d
Hoonssac <Hoonssac.dev@gmail.com>
2025-06-11 15:44:15 +0900

    Add ControllerAdvice annotation

--- KB-Spring/Spring/MyBatisSpringLegacy/src/main/java/org/scoula/exception/CommonExceptionAdvice.java
+++ KB-Spring/Spring/MyBatisSpringLegacy/src/main/java/org/scoula/exception/CommonExceptionAdvice.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
+@ControllerAdvice
 public class CommonExceptionAdvice {
 	@ExceptionHandler(Exception.class)
 	public String except(Exception ex, Model model) {

40d0e31e04e59964445d6aed9331637b9dc6894e
Hoonssac <Hoonssac.dev@gmail.com>
2025-06-11 15:31:07 +0900

    Add exception handling configuration and global error structure

--- KB-Spring/Spring/SpringLegacy/src/main/java/org/scoula/config/ServletConfig.java
+++ KB-Spring/Spring/SpringLegacy/src/main/java/org/scoula/config/ServletConfig.java
@@ -1,6 +1,9 @@
 package org.scoula.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -30,5 +33,11 @@ public class ServletConfig implements WebMvcConfigurer {
 		bean.setSuffix(".jsp");
 
 		registry.viewResolver(bean);
+	}
+
+	@Bean
+	public MultipartResolver multipartResolver() {
+		StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
+		return resolver;
 	}
 }
--- KB-Spring/Spring/ex03/src/main/java/org/scoula/exception/CommonExceptionAdvice.java
+++ KB-Spring/Spring/ex03/src/main/java/org/scoula/exception/CommonExceptionAdvice.java
@@ -1,5 +1,7 @@
 package org.scoula.exception;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,23 +11,25 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.log4j.Log4j2;
 
-@ControllerAdvice
+@ControllerAdvice // 모든 컨트롤러에서 발생하는 예외 처리
 @Log4j2
 public class CommonExceptionAdvice {
 
-	@ExceptionHandler(Exception.class)
+	@ExceptionHandler(Exception.class) // 모든 예외 처리
 	public String except(Exception ex, Model model) {
 		log.error("Exception......." + ex.getMessage());
+		// Model로 예외 객체를 뷰에 전달
 		model.addAttribute("exception", ex);
 		log.error("model");
 		return "error_page";
 	}
 
-	@ExceptionHandler(NoHandlerFoundException.class)
+	@ExceptionHandler(NoHandlerFoundException.class) // 404 요청 전용 처리
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public String handle404(NoHandlerFoundException ex) {
+	public String handle404(NoHandlerFoundException ex, Model model, HttpServletRequest request) {
 		log.error(ex);
-		// model.addAttribute("uri", request.getRequestURI());
+		// 어떤 경로에서 404가 발생했는지 뷰에 전달
+		model.addAttribute("uri", request.getRequestURI());
 		return "custom404";
 	}
 }

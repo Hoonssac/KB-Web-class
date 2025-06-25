45b8d836dbb2821547eb1ce9f5637e03bdf03a6d
Hoonssac <Hoonssac.dev@gmail.com>
2025-06-25 14:55:42 +0900

    feat: Implement JWT authentication with Spring Security

--- KB-Spring/SpringSecurity/apiserver/src/main/java/org/scoula/security/config/SecurityConfig.java
+++ KB-Spring/SpringSecurity/apiserver/src/main/java/org/scoula/security/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package org.scoula.security.config;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.scoula.security.filter.AuthenticationErrorFilter;
+import org.scoula.security.filter.JwtAuthenticationFilter;
 import org.scoula.security.filter.JwtUsernamePasswordAuthenticationFilter;
+import org.scoula.security.handler.CustomAccessDeniedHandler;
+import org.scoula.security.handler.CustomAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,6 +43,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	// DB 기반 인증 처리를 위한 서비스 (in-memory 방식과 병행 불가)
 	private final UserDetailsService userDetailsService;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final AuthenticationErrorFilter authenticationErrorFilter;
+
+	private final CustomAccessDeniedHandler accessDeniedHandler;
+	private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
 	@Autowired
 	private JwtUsernamePasswordAuthenticationFilter jwtUsernamePasswordAuthenticationFilter;
@@ -54,10 +64,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		// 한글 인코딩 필터 설정
 		http.addFilterBefore(encodingFilter(), CsrfFilter.class)
-
+			// 인증 에러 필터
+			.addFilterBefore(authenticationErrorFilter, UsernamePasswordAuthenticationFilter.class)
+			// Jwt 인증 필터
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 			// 로그인 인증 필터
 			.addFilterBefore(jwtUsernamePasswordAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+		// 예외 처리 설정
+		http
+			.exceptionHandling()
+			.authenticationEntryPoint(authenticationEntryPoint)
+			.accessDeniedHandler(accessDeniedHandler);
+
+		http
+			.authorizeRequests() // 경로별 접근 권한 설정
+			.antMatchers(HttpMethod.OPTIONS).permitAll()
+			.antMatchers("/api/security/all").permitAll() // 모두 허용
+			.antMatchers("/api/security/member").access("hasRole('ROLE_MEMBER')") // ROLE_MEMBER 이상 접근 허용
+			.antMatchers("/api/security/admin").access("hasRole('ROLE_ADMIN')") // ROLD_ADMIN 이상 접근 허용
+			.anyRequest().authenticated(); // 나머지는 로그인 된 경우 모두 허용
 
 		http.httpBasic().disable() // 기본 HTTP 인증 비활성화
 			.csrf().disable() // csrf 비활성화
